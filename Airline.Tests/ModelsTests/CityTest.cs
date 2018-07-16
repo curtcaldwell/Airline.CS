d54f7fccf7305e03ca0b250d66b3b75eaa2371d9
Guest User <Guest@epicodus-31.local>
2018-07-16 11:18:38 -0700

    Make folders and files for airline app

---
+++ Airline.Tests/ModelsTests/CityTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Airline.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Airline.Tests
+{
+  [TestClass]
+  public class CityTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ Airline/Models/Flight.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+using Airline;
+
+namespace Airline.Models
+{
+  public class Flight
+  {
+    public Flight ()
+    {
+
+    }
+  }
+}
